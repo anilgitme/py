579acef895e60c111ac4c8dec6a9ebc2ad4328bc
Anil_Ramcharran <anil.gitme@gmail.com>
2021-10-27 22:03:25 -0400

    reverse words in javascript

---
+++ Javascript/reverseWords.js
@@ -0,0 +1,15 @@
+var reverseWords = function(s) {
+    let words = s.split(' ');
+    let rev_words = [];
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i] !== '') {
+            rev_words.push(words[i])
+        }
+    };
+
+    return rev_words.join(' ');
+};
+
+
+console.log(reverseWords('the sky is blue'))
--- reverseWords.py
+++ reverseWords.py
@@ -2,8 +2,13 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         words = s.split(' ')
         rev_words = []
-        
+        if len(words) <= 1:
+            return ' '.join(words)
         for i in range(len(words)-1, -1, -1):
             if words[i] != '':
                 rev_words.append(words[i])  
         return ' '.join(rev_words)
+
+
+
+
