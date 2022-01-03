711b8ff2cf36ca29136aab4061d930786f49ed6a
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-03 17:02:16 -0500

    tests for contains duplicate

--- containsDuplicates.py
+++ containsDuplicates.py
@@ -8,3 +8,27 @@ class Solution:
             else:
                 return True
         return False
+
+# brute force
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool: 
+        size = len(nums)
+        if size <= 1:
+            return False
+        
+        for i in range(0, size):
+            for j in range(i + 1, size):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+        
+        
+        
+A = Solution()
+print(A.containsDuplicate([3,0,1,4,1,5]))
+print(A.containsDuplicate([0,1,2,3,4,4]))
+print(A.containsDuplicate([0,1,2,3,4]))
+print(A.containsDuplicate([-6,5,6,1,8,-3,0]))
+print(A.containsDuplicate([]))
+print(A.containsDuplicate([-1]))
