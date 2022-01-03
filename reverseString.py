7399f28feee880e8ed1a3a06914a45976135201a
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-03 18:28:44 -0500

    tests for reversing a list

--- reverseString.py
+++ reverseString.py
@@ -15,3 +15,28 @@ print(A.reverseString(["h","e","l","l","o"]))
 print(A.reverseString([""]))
 print(A.reverseString(["s"]))
 print(A.reverseString(["s","s","s"]))
+
+
+# 2
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        j = len(s) - 1
+        if not s:
+            return s
+        
+        for i in range(len(s)):
+            if i >= j:
+                break
+            s[i], s[j] = s[j], s[i]
+            j -= 1
+        return s
+
+
+obj = Solution()
+print(obj.reverseString(["h","e","l","l","o"]))
+print(obj.reverseString([]))
+print(obj.reverseString(["R","o","t","o","r"]))
+print(obj.reverseString(["z"]))
+print(obj.reverseString(["E","e"]))
