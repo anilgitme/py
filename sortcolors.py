22c3f6fe26a887e7004027219f19a175b43ab9e3
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-05 18:12:22 -0500

    tests for sorting unique elements

--- sortcolors.py
+++ sortcolors.py
@@ -22,3 +22,37 @@ class Solution:
             else:
                 nums[i], nums[k] = nums[k], nums[i]
                 k -= 1
+
+
+# 2
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        j = 0
+        k = len(nums) - 1
+        i = 0
+        if len(nums) <= 1:
+            return nums
+        
+        while i <= k:
+            
+            if nums[i] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[k] = nums[k], nums[i]
+                k -= 1
+            else:
+                i += 1
+        return nums
+        
+
+A = Solution()
+print(A.sortColors([2,0,2,1,1,0]))
+print(A.sortColors([0,2]))
+print(A.sortColors([2,0,1]))
+print(A.sortColors([1]))
+print(A.sortColors([1,0,0,0,1]))
+print(A.sortColors([]))
