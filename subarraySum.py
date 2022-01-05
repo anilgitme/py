43097bd402cbf6b279315886a85ab20b481ad576
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-05 18:55:12 -0500

    tests for continious subArray sum

--- subarraySum.py
+++ subarraySum.py
@@ -22,3 +22,37 @@ print(A.subarraySum([1,1,1], 2))
 print(A.subarraySum([], 0))
 print(A.subarraySum([-1,-2,-3,-4], -1))
 print(A.subarraySum([5,5,5,5], 5)) 
+
+
+#2
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        seen = {0:1}
+        cur_Sum = 0
+        
+        if not nums:
+            return nums
+        
+        for num in nums:
+            cur_Sum += num
+            
+            diff = cur_Sum - k
+            
+            if diff in seen:
+                count += seen[diff]
+                
+            if cur_Sum not in seen:
+                seen[cur_Sum] = 1
+            else:
+                seen[cur_Sum] += 1
+        return count
+
+
+obj = Solution()
+print(obj.subarraySum([1,1,1], 2))
+print(obj.subarraySum([1], 1))
+print(obj.subarraySum([], 2))
+print(obj.subarraySum([1,1,1], 1))
+print(obj.subarraySum([0,2,5,7], 0))
