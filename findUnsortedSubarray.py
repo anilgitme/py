c6e23fa5a54ae327b97f967fe61369ce741c2617
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-04 19:58:20 -0500

    tests for finding unsorted subarray length

--- findUnsortedSubarray.py
+++ findUnsortedSubarray.py
@@ -21,3 +21,41 @@ print(A.findUnsortedSubarray([2,6,4,8,10,9,15]))
 print(A.findUnsortedSubarray([]))
 print(A.findUnsortedSubarray([1,2,3,4]))
 print(A.findUnsortedSubarray([1,6,4,5,7]))
+
+
+
+#2
+
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        leftNum = float('-inf')
+        rightNum = float('inf')
+        rightIdx = 0
+        leftIdx = 0
+        
+        for i in range(len(nums)):
+            if nums[i] >= leftNum:
+                leftNum = nums[i]
+            else:
+                rightIdx = i
+        
+
+        for j in range(len(nums) -1, -1, -1):
+            if nums[j] <= rightNum:
+                rightNum = nums[j]
+            else:
+                leftIdx = j
+        if rightIdx > leftIdx:
+            return rightIdx - leftIdx + 1
+        else:
+            return 0
+            
+            
+        
+        
+A = Solution()
+print(A.findUnsortedSubarray([2,6,4,8,10,9,15]))
+print(A.findUnsortedSubarray([]))
+print(A.findUnsortedSubarray([5]))
+print(A.findUnsortedSubarray([0,3,9,8,6,5,10]))
+print(A.findUnsortedSubarray([2,2,3,5,4,7,10]))
