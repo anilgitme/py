5d1b28792420620562cc7eec2d778be8d9724f03
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-06 17:18:24 -0500

    tests for search

--- binarySearch.py
+++ binarySearch.py
@@ -20,3 +20,32 @@ print(A.search([-1,0,3,5,9,12],9))
 print(A.search([0], 1))
 print(A.search([-1,-2], -2))
 print(A.search([12,18,99], 99))
+
+
+#2
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        size = len(nums)
+        left = 0
+        right = size - 1
+        if size == 0:
+            return - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return - 1
+    
+obj = Solution()
+print(obj.search([-1,0,3,5,9,12], 9))
+print(obj.search([-9,-6,-5,-1,1,5], -5))
+print(obj.search([1,8,9,99], 10))
+print(obj.search([], 0))
+print(obj.search([2], 2))
