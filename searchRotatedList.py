f0f80f138b749dab867f0ecaf60c90b752a38c9d
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-06 19:50:59 -0500

    tests for searching a rotated array

--- searchRotatedList.py
+++ searchRotatedList.py
@@ -29,3 +29,43 @@ print(A.searchRotatedList([4,5,6,7,0,1,2], 0))
 print(A.searchRotatedList([2], 2)) 
 print(A.searchRotatedList([4,5,6,7,0,1,2], 9)) 
 print(A.searchRotatedList([], 0)) 
+
+
+
+#2
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        
+        if not nums:
+            return -1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if target == nums[mid]:
+                return mid
+            
+            if nums[mid] >= nums[l]: 
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                
+        return -1
+
+
+obj = Solution()
+
+print(obj.search([[4,5,6,7,0,1,2]], 0))
+print(obj.search([[4,5,6,7,-1,-2,-3]], -3))
+print(obj.search([[]], 0))
+print(obj.search([[1]], -1))
