af2a43424ca315713ab58588054cfdb09c968044
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-07 22:09:25 -0500

    tests for searchRange

--- searchRange.py
+++ searchRange.py
@@ -1,12 +1,10 @@
 class Solution:
-    def searchCountTarget(self, nums: List[int], target: int) -> List[int]:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
         left = 0
         right = len(nums) - 1
-        
-        if len(nums) == 0:
-            return 0
-        elif len(nums) == 1 and target == nums[0]:
-            return 1
+        res = [-1, -1]
+        if not nums:
+            return res
         
         while left <= right:
             mid = (left + right) // 2
@@ -16,18 +14,18 @@ class Solution:
                 right = mid - 1
             else:
                 if nums[left] == target and nums[right] == target:
-                    return right - left + 1
+                    res = [left, right]
                     break
                 if nums[left] < target:
                     left += 1
                     
                 elif nums[right] > target:
                     right -= 1
-        return 0
-
-A = Solution()   
-print(A.searchCountTarget([4, 4, 8, 8, 8, 15, 16, 23, 23, 42], 8))
-print(A.searchCountTarget([3,5,6,9], 7))
-print(A.searchCountTarget([], 8))
-print(A.searchCountTarget([8], 8))
-print(A.searchCountTarget([5,5,5,5,5,5], 5))
+        return res
+        
+A = Solution()
+print(A.searchRange([5,7,7,8,8,10],8))
+print(A.searchRange([],0))
+print(A.searchRange([1],1))
+print(A.searchRange([-2,-2,0,0,5,5,6],-2))
+print(A.searchRange([0,0,0,0],0))
