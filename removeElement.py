42c48f389b6b3b4610187c766e1352f6429684e3
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-09 22:06:20 -0500

    tests for removing elements

--- removeElement.py
+++ removeElement.py
@@ -24,3 +24,24 @@ print(A.removeElement([3,2,2,3], 3))
 print(A.removeElement([], 2))
 print(A.removeElement([0,1,2,2,3,0,4,2], 2))
 print(A.removeElement([2,2,2,2,2,2,2], 2))
+
+
+#2
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.remove(val)
+                continue
+            i += 1
+        return len(nums)
+        
+        
+A = Solution()
+print(A.removeElement([0,1,2,2,3,0,4,2], 2))
+print(A.removeElement([0], 0))
+print(A.removeElement([5], 2))
+print(A.removeElement([-1,-1,-5,-6,-3,-5,0], -5))
+print(A.removeElement([], 1))
