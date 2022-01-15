c7f75a9350029baa9ba9372804d4c58e4b33d4ca
Anil_Ramcharran <anil.gitme@gmail.com>
2022-01-14 23:07:45 -0500

    tests for twoSum

--- twoSum.py
+++ twoSum.py
@@ -23,3 +23,31 @@ print(A.twoSum([], 0))
 print(A.twoSum([-4,-3,-2,-1,0,1,2], -6))
 print(A.twoSum([-1,0], -1))
 print(A.twoSum([3,4,8,7,9], 9))
+
+
+#2
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        left = 0
+        right = len(numbers) - 1
+        
+        while left <= right:
+            total = numbers[left] + numbers[right]
+            
+            if total == target:
+                return [left + 1, right + 1]
+            elif target > total:
+                left += 1
+            else:
+                right -= 1
+        return -1
+        
+        
+obj = Solution()        
+print(obj.twoSum([2,7,11,15],9))
+print(obj.twoSum([1],1))
+print(obj.twoSum([1,1,1],2))
+print(obj.twoSum([-3,-1,0,2,5,7],4))
+print(obj.twoSum([],0))
